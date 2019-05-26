22135619d142d7ffe0ab590224e259eb05514978
Helio Rodrigues <hlalvesbr@gmail.com>
2019-05-26 17:49:46 -0300

    Teste criar anuncio

--- app/src/main/java/com/google/android/gms/nearby/messages/samples/nearbydevices/TelaCriaAnuncio.java
+++ app/src/main/java/com/google/android/gms/nearby/messages/samples/nearbydevices/TelaCriaAnuncio.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class TelaCriaAnuncio extends AppCompatActivity {
 
@@ -11,6 +14,18 @@ public class TelaCriaAnuncio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criaranuncio);
+        Button salvarButton = (Button) findViewById(R.id.salvarButton);
 
+        salvarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSalvarAnuncio();
+            }
+        });
+    }
+
+    public void onSalvarAnuncio() {
+        EditText nomeAnuncioText = (EditText) findViewById(R.id.nomeText);
+        
     }
 }
