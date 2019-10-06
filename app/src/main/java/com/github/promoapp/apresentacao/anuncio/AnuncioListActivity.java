4f92abe504b85951dc650725b0ea5fd5c8f4b16d
Hélio Bessoni Rodrigues <hlalvesbr@gmail.com>
2019-10-06 16:30:16 -0300

    Lista anuncios.

--- app/src/main/java/com/github/promoapp/apresentacao/anuncio/AnuncioListActivity.java
+++ app/src/main/java/com/github/promoapp/apresentacao/anuncio/AnuncioListActivity.java
@@ -2,14 +2,18 @@ package com.github.promoapp.apresentacao.anuncio;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,14 +21,30 @@ import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import com.github.promoapp.R;
+import com.github.promoapp.apresentacao.MainActivity;
 import com.github.promoapp.dominio.anuncio.Anuncio;
+import com.github.promoapp.dominio.anuncio.AnuncioMessage;
 import com.github.promoapp.dominio.anuncio.AnuncioRepository;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessagesOptions;
+import com.google.android.gms.nearby.messages.NearbyPermissions;
+import com.google.android.gms.nearby.messages.PublishCallback;
+import com.google.android.gms.nearby.messages.PublishOptions;
+import com.google.android.gms.nearby.messages.Strategy;
 
 import java.util.List;
+import java.util.UUID;
 
-public class AnuncioListActivity extends AppCompatActivity {
+public class AnuncioListActivity extends AppCompatActivity
+        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private boolean mTwoPane;
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,6 +93,45 @@ public class AnuncioListActivity extends AppCompatActivity {
         View recyclerView = findViewById(R.id.anuncio_list);
         assert recyclerView != null;
         setupRecyclerView((RecyclerView) recyclerView);
+        buildGoogleApiClient();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i(getClass().getSimpleName(), "GoogleApiClient connected");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        logAndShowSnackbar("Connection suspended. Error code: " + i);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        logAndShowSnackbar("Exception while connecting to Google Play services: " +
+                connectionResult.getErrorMessage());
+    }
+
+    private void logAndShowSnackbar(final String text) {
+        Log.w(getClass().getSimpleName(), text);
+        View container = findViewById(R.id.activity_main_container);
+
+        if (container != null) {
+            Snackbar.make(container, text, Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+    private void buildGoogleApiClient() {
+        if (mGoogleApiClient != null) {
+            return;
+        }
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Nearby.MESSAGES_API, new MessagesOptions.Builder()
+                        .setPermissions(NearbyPermissions.BLE).build())
+                .addConnectionCallbacks(this)
+                .enableAutoManage(this, this)
+                .build();
     }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
@@ -135,7 +194,8 @@ public class AnuncioListActivity extends AppCompatActivity {
             View view = LayoutInflater.from(parent.getContext()).inflate(
                     R.layout.anuncio_list_content, parent, false);
 
-            return new AnuncioViewHolder(view);
+            return new AnuncioViewHolder(view, mParentActivity,
+                    mParentActivity.mGoogleApiClient);
         }
 
         @Override
@@ -154,12 +214,28 @@ public class AnuncioListActivity extends AppCompatActivity {
 
         class AnuncioViewHolder extends RecyclerView.ViewHolder {
 
+            private static final String KEY_UUID = "key_uuid";
+            private static final int TTL_IN_SECONDS = 3 * 60; // Three minutes.
+
+            private final Strategy PUB_SUB_STRATEGY = (new Strategy.Builder()).zze(2)
+                    .setTtlSeconds(TTL_IN_SECONDS).build();
+
             TextView mIdView;
             TextView mContentView;
             SwitchCompat mPublishSwitch;
+            GoogleApiClient mGoogleApiClient;
+            View mView;
+            AnuncioListActivity mActivity;
+            Message mPubMessage;
 
-            AnuncioViewHolder(View view) {
+            AnuncioViewHolder(View view, AnuncioListActivity activity,
+                              GoogleApiClient googleApiClient) {
+
                 super(view);
+
+                mView = view;
+                mActivity = activity;
+                mGoogleApiClient = googleApiClient;
                 mIdView = view.findViewById(R.id.id_text);
                 mContentView = view.findViewById(R.id.content);
                 mPublishSwitch = view.findViewById(R.id.publish_switch);
@@ -167,9 +243,6 @@ public class AnuncioListActivity extends AppCompatActivity {
                 mPublishSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                     @Override
                     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        // If GoogleApiClient is connected, perform pub actions in response to user action.
-                        // If it isn't connected, do nothing, and perform pub actions when it connects (see
-                        // onConnected()).
                         if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
                             if (isChecked) {
                                 publish();
@@ -179,6 +252,69 @@ public class AnuncioListActivity extends AppCompatActivity {
                         }
                     }
                 });
+            }
+
+            private void publish() {
+                mPubMessage = AnuncioMessage.newNearbyMessage(getUUID(mActivity.getSharedPreferences(
+                                mActivity.getApplicationContext().getPackageName(),
+                                Context.MODE_PRIVATE)));
+
+                Log.i(getClass().getSimpleName(), "Publishing");
+
+                PublishOptions options = new PublishOptions.Builder()
+                        .setStrategy(PUB_SUB_STRATEGY)
+                        .setCallback(new PublishCallback() {
+                            @Override
+                            public void onExpired() {
+                                super.onExpired();
+                                Log.i(getClass().getSimpleName(), "No longer publishing");
+
+                                mActivity.runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        mPublishSwitch.setChecked(false);
+                                    }
+                                });
+                            }
+                        }).build();
+
+                Nearby.Messages.publish(mGoogleApiClient, mPubMessage, options)
+                        .setResultCallback(new ResultCallback<Status>() {
+                            @Override
+                            public void onResult(@NonNull Status status) {
+                                if (status.isSuccess()) {
+                                    Log.i(getClass().getSimpleName(), "Published successfully.");
+                                } else {
+                                    logAndShowSnackbar("Could not publish, status = " + status);
+                                    mPublishSwitch.setChecked(false);
+                                }
+                            }
+                        });
+            }
+
+            private String getUUID(SharedPreferences sharedPreferences) {
+                String uuid = sharedPreferences.getString(KEY_UUID, "");
+
+                if (TextUtils.isEmpty(uuid)) {
+                    uuid = UUID.randomUUID().toString();
+                    sharedPreferences.edit().putString(KEY_UUID, uuid).apply();
+                }
+
+                return uuid;
+            }
+
+            private void unpublish() {
+                Log.i(getClass().getSimpleName(), "Unpublishing.");
+                Nearby.Messages.unpublish(mGoogleApiClient, mPubMessage);
+            }
+
+            private void logAndShowSnackbar(final String text) {
+                Log.w(getClass().getSimpleName(), text);
+                View container = mView.findViewById(R.id.activity_main_container);
+
+                if (container != null) {
+                    Snackbar.make(container, text, Snackbar.LENGTH_LONG).show();
+                }
             }
         }
     }
